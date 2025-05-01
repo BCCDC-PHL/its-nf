2de1791a184f049aad107ae98b412af66488ef6f
John Palmer <john.palmer@bccdc.ca>
2025-04-30 22:35:22 -0700

    rename functions

--- bin/filter_best_bitscore.py
+++ bin/filter_best_bitscore.py
@@ -7,14 +7,14 @@ import sys
 import pandas as pd 
 
 
-def filter_best_bitscore(df, group_col, score_col):
+def filter_dataframe(df, group_col, score_col):
     idxmax = df.groupby(group_col)[score_col].idxmax()
     df = df.loc[idxmax].reset_index(drop=True)
     return df
 
 def main(args):
     blast_df = pd.read_csv(args.input)
-    filtered_df = filter_best_bitscore(blast_df, args.group_col, args.score_col)
+    filtered_df = filter_dataframe(blast_df, args.group_col, args.score_col)
     filtered_df.to_csv(args.output, index=False)
 
 
